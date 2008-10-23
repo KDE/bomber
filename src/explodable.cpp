a89937cd16ea440e44566e007b4835a51db9cabe
John-Paul Stanford <jp@stanwood.org.uk>
2008-10-23 19:38:48 +0000

    Code style clean ups
    svn path=/trunk/kdereview/bomber/; revision=875242

--- src/explodable.cpp
+++ src/explodable.cpp
@@ -39,8 +39,9 @@ Explodable::Explodable(QString mainSvg, QString explosionSvg,
 		qreal relativeWidth, qreal relativeHeight, BomberRenderer* renderer,
 		BomberBoard* board) :
 	KGameCanvasPixmap(board), m_renderer(renderer), m_board(board), m_mainSvg(
-			mainSvg), m_explosionSvg(explosionSvg),m_size(32, 64)
+			mainSvg), m_explosionSvg(explosionSvg)
 {
+	m_size = QSize(32, 64);
 	m_relativeWidth = relativeWidth;
 	m_relativeHeight = relativeHeight;
 	resetPixmaps();
