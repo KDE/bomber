d5a80cb5fc2a5bd25fa28163ae4a6a5dbf0cee0c
Albert Astals Cid <tsdgeos@terra.es>
2009-01-05 14:11:38 +0000

    there are no ui files anymore
    svn path=/trunk/KDE/kdegames/bomber/; revision=906085

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-$EXTRACTRC `find -name \*.ui` >> rc.cpp || exit 11
+# $EXTRACTRC `find -name \*.ui` >> rc.cpp || exit 11
 $EXTRACTRC `find . -name "*.rc"` >> rc.cpp || exit 12
 $XGETTEXT rc.cpp `find . -name '*.cpp'` -o $podir/bomber.pot
 rm -f rc.cpp
